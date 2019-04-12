6ef4696c3397f9c6a12385cf565e14bd469aa838
Amriesgrace <obieri.grace@gmail.com>
2019-04-12 16:03:59 +0100

    Updates

---
+++ precache-manifest.a8a8f32afb80181634b33998d2c8a56a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "066cb11832e18efafa24",
+    "url": "/react-calendar-app/static/js/runtime~main.f9b172e6.js"
+  },
+  {
+    "revision": "765c202bc7559f06829b",
+    "url": "/react-calendar-app/static/js/main.602bc59a.chunk.js"
+  },
+  {
+    "revision": "a7420fe17dd8b13729fd",
+    "url": "/react-calendar-app/static/js/2.1bff30b4.chunk.js"
+  },
+  {
+    "revision": "765c202bc7559f06829b",
+    "url": "/react-calendar-app/static/css/main.785c995c.chunk.css"
+  },
+  {
+    "revision": "9cc52943ef07e365a5f1fb4b6329292b",
+    "url": "/react-calendar-app/index.html"
+  }
+];
